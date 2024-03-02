8f684629523e982475996dc7294935e332ba3bd3
Rohit Balage <thatcomputerboy26@gmail.com>
2024-03-02 11:21:56 -0500

    initial setup

---
+++ app/src/main/java/com/rrbofficial/journalappkotlin/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.rrbofficial.journalappkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// this is highlight
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
