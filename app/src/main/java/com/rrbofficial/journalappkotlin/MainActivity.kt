3695221997aca5cb8dbc9177ee5d2dc80f45b63e
Rohit Balage <thatcomputerboy26@gmail.com>
2024-03-02 16:53:30 -0500

    added data bindig and enabled firebase auth

--- app/src/main/java/com/rrbofficial/journalappkotlin/MainActivity.kt
+++ app/src/main/java/com/rrbofficial/journalappkotlin/MainActivity.kt
@@ -1,13 +1,35 @@
 package com.rrbofficial.journalappkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.rrbofficial.journalappkotlin.databinding.ActivityMainBinding
 
 // this is highlight
 
+
 class MainActivity : AppCompatActivity() {
+
+    lateinit var  binding : ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.createAcctBTN.setOnClickListener()
+        {
+            val intent = Intent (this, SignUpActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
+
 }
--- app/src/main/java/com/rrbofficial/journalappkotlin/SignUpActivity.kt
+++ app/src/main/java/com/rrbofficial/journalappkotlin/SignUpActivity.kt
@@ -2,10 +2,85 @@ package com.rrbofficial.journalappkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
+import com.rrbofficial.journalappkotlin.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    // Binding
+    lateinit var  binding : ActivitySignUpBinding
+
+
+    // Firebase Auth
+    private lateinit var auth: FirebaseAuth
+
+            override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+                // Binding
+
+                binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_up)
+
+
+                // Initialize firebase auth
+                auth = Firebase.auth
+
+
+                // Sign up with SignUp button
+                binding.accSignUpButton.setOnClickListener()
+                {
+                    createUser()
+                }
+    }
+
+    private fun createUser() {
+
+        val email = binding.emailCreate.text.toString()
+        val password = binding.passwordCreate.text.toString()
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAGY", "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAGY", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+
+    }
+
+
+    // Checks whether the user signed in or not.
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+           reload()
+        }
+    }
+
+    private fun reload() {
+        TODO("Not yet implemented")
     }
 }
